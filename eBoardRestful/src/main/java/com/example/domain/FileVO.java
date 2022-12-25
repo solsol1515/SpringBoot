49db06e6ee49aa7e9c77373d7eee084649330c4e
solsol1515 <112688276+solsol1515@users.noreply.github.com>
2022-12-25 22:56:50 +0900

    Update FileVO.java

--- eBoardRestful/src/main/java/com/example/domain/FileVO.java
+++ eBoardRestful/src/main/java/com/example/domain/FileVO.java
@@ -6,9 +6,9 @@ import lombok.Data;
 @Data
 public class FileVO {
 
-	private Long 	fileId;			// 파일 아이디?
+	private Long 	fileId;		// 파일 아이디?
 	private String 	origFilename;	// 원래 파일 이름?
-	private String 	filename;		// 파일 이름
-	private String 	filePath;		// 파일 경로
+	private String 	filename;	// 파일 이름
+	private String 	filePath;	// 파일 경로
 	
 }
